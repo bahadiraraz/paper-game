fda691c7e2cb563da3c3312322098701c25b0d44
Bahadır Araz <b123qweasdzxc@gmail.com>
2023-02-15 13:03:35 +0300

    add ia

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import sys
 
 import numpy as np
 import pygame
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import Adam
 from pydantic import BaseModel, Field
 
 
@@ -11,10 +14,8 @@ class MapConfig(BaseModel):
     top_right_color: list = Field([0, 255, 0])
     bottom_left_color: list = Field([0, 150, 255])
     bottom_right_color: list = Field([255, 215, 0])
-    window_size: int = Field(500, ge=1, le=500)
-    grid_size: int = Field(100, ge=1, le=100)
-    player_size: int = Field(5, ge=1, le=10)
-    scale: int = Field(1, ge=1, le=20)
+    window_size: int = Field(1000, ge=1, le=1000)
+    player_size: int = Field(10, ge=1, le=10)
     caption: str = Field("map")
     font_size: int = Field(20, ge=1, le=100)
     font_type: str = Field("arial")
@@ -23,17 +24,51 @@ class MapConfig(BaseModel):
 pygame.init()
 MapConfig = MapConfig()
 
-screen = pygame.display.set_mode((MapConfig.window_size, MapConfig.window_size))
-pygame.display.set_caption(MapConfig.caption)
-top_left_color = MapConfig.top_left_color
-top_right_color = MapConfig.top_right_color
-bottom_left_color = MapConfig.bottom_left_color
-bottom_right_color = MapConfig.bottom_right_color
+grid_size = 1000
+player_size = MapConfig.player_size
+
+if grid_size % player_size != 0:
+    grid_size += player_size - (grid_size % player_size)
+
 grid = np.full(
-    (MapConfig.grid_size * MapConfig.scale, MapConfig.grid_size * MapConfig.scale, 3),
+    (grid_size, grid_size, 3),
     (255, 255, 255),
 )
+screen_size = grid_size * player_size
+screen = pygame.display.set_mode((MapConfig.window_size, MapConfig.window_size))
+pygame.display.set_caption(MapConfig.caption)
+
 font = pygame.font.SysFont(MapConfig.font_type, MapConfig.font_size)
+
+grid_size = 80
+color_size = int(grid_size * 0.06)
+
+for i in range(grid_size):
+    for j in range(grid_size):
+        if (i > color_size * 2 and i < color_size * 3) and (
+            j > color_size * 2 and j < color_size * 3
+        ):
+            grid[i][j] = MapConfig.top_left_color
+        elif (i > color_size * 6 and i < color_size * 7) and (
+            j > color_size * 2 and j < color_size * 3
+        ):
+            grid[i][j] = MapConfig.top_right_color
+        elif (i > color_size * 2 and i < color_size * 3) and (
+            j > color_size * 6 and j < color_size * 7
+        ):
+            grid[i][j] = MapConfig.bottom_left_color
+        elif (i > color_size * 6 and i < color_size * 7) and (
+            j > color_size * 6 and j < color_size * 7
+        ):
+            grid[i][j] = MapConfig.bottom_right_color
+
+
+player_size = 5
+actions = [
+    (x, y, x + player_size - 1, y + player_size - 1)
+    for x in range(0, grid_size - player_size + 1, player_size)
+    for y in range(0, grid_size - player_size + 1, player_size)
+]
 
 
 def get_color_name(color):
@@ -47,72 +82,230 @@ def get_color_name(color):
         return "Yellow"
 
 
-grid_size = MapConfig.grid_size * MapConfig.scale
-color_size = int(grid_size * 0.06)
-player_size = MapConfig.player_size * MapConfig.scale
-
-for i in range(grid_size):
-    for j in range(grid_size):
-        if (i > color_size * 2 and i < color_size * 3) and (
-            j > color_size * 2 and j < color_size * 3
-        ):
-            grid[i][j] = top_left_color
-        elif (i > color_size * 6 and i < color_size * 7) and (
-            j > color_size * 2 and j < color_size * 3
-        ):
-            grid[i][j] = top_right_color
-        elif (i > color_size * 2 and i < color_size * 3) and (
-            j > color_size * 6 and j < color_size * 7
-        ):
-            grid[i][j] = bottom_left_color
-        elif (i > color_size * 6 and i < color_size * 7) and (
-            j > color_size * 6 and j < color_size * 7
-        ):
-            grid[i][j] = bottom_right_color
-
-
 running = True
 dragging = False
 start_pos = None
 end_pos = None
 
 
-def bot_function(color):
-    color_indices = np.where(np.all(grid == color, axis=-1))
-    color_zone = np.column_stack(color_indices)
+def get_state(grid, color):
+    color_mask = (grid == color).all(axis=2)
+    color_total_area = np.sum(color_mask)
+    if color_total_area == 0:
+        return [0, 0, 0]
+    color_mean = np.mean(grid[color_mask], axis=0) / 255
+    opponent_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    opponent_total_area = 0
+    for opponent_color in opponent_colors:
+        opponent_mask = (grid == opponent_color).all(axis=2)
+        opponent_total_area += np.sum(opponent_mask)
+    return [
+        color_total_area / (100 * 100),
+        color_mean[0],
+        color_mean[1],
+        color_mean[2],
+        opponent_total_area / (100 * 100),
+    ]
 
-    if color_zone.size == 0:
-        print(f"No {color} zone found.")
-        return
 
-    start_pos = color_zone[np.random.randint(color_zone.shape[0])]
-    start_x, start_y = start_pos
-    end_x = np.clip(start_x + np.random.randint(-10, 11), 0, 49)
-    end_y = np.clip(start_y + np.random.randint(-10, 11), 0, 49)
-    end_pos = (end_x, end_y)  # noqa F841
+def ai_function(color, grid):
+    while True:
+        color_mask = (grid == color).all(axis=2)
+        color_total_area = np.sum(color_mask)
+        if color_total_area == 0:
+            print(f"No {color} zone found.")
+            continue
 
-    start_x, end_x = np.sort([start_x, end_x])
-    start_y, end_y = np.sort([start_y, end_y])
-    selected_area = (end_x - start_x + 1) * (end_y - start_y + 1)
-    total_color_square = np.sum((grid == color).all(axis=2))
-
-    if selected_area <= total_color_square:
-        probability = random.random()
-        if probability <= 0.9:
-            grid[start_x : end_x + 1, start_y : end_y + 1] = color
+        if color_total_area > (grid.shape[0] * grid.shape[1]) / 2:
+            opponent_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+            min_opponent_area = float("inf")
+            min_opponent_pos = None
+            for opponent_color in opponent_colors:
+                opponent_mask = (grid == opponent_color).all(axis=2)
+                opponent_area = np.sum(opponent_mask)
+                if opponent_area < min_opponent_area:
+                    opponent_indices = np.argwhere(opponent_mask)
+                    color_indices = np.argwhere(color_mask)
+                    distances = np.sqrt(
+                        np.sum(
+                            (opponent_indices[:, np.newaxis, :] - color_indices) ** 2,
+                            axis=-1,
+                        )
+                    )
+                    min_distance = np.min(distances)
+                    if np.isfinite(min_distance):
+                        min_opponent_area = opponent_area
+                        min_opponent_pos = opponent_indices[np.argmin(distances)]
+            if min_opponent_pos is not None:
+                start_x, start_y = min_opponent_pos
+                max_area = min_opponent_area
+                print(f"{color} zone is too big. Starting to eat smaller areas.")
+            else:
+                start_pos = np.argwhere(color_mask)[np.random.randint(color_total_area)]
+                start_x, start_y = start_pos
+                max_area = color_total_area
         else:
+            start_pos = np.argwhere(color_mask)[np.random.randint(color_total_area)]
+            start_x, start_y = start_pos
+            max_area = color_total_area
+
+        end_x = np.clip(start_x + np.random.randint(1, max_area + 1), 0, 99)
+        end_y = np.clip(start_y + np.random.randint(1, max_area + 1), 0, 99)
+
+        start_x, end_x = np.sort([start_x, end_x])
+        start_y, end_y = np.sort([start_y, end_y])
+        selected_area = (end_x - start_x + 1) * (end_y - start_y + 1)
+
+        if selected_area > color_total_area:
+            print("Selected area is greater than the maximum available area.")
+            continue
+        elif selected_area <= 0:
+            print("Selected area cannot be zero.")
+            continue
+
+        grid_mask = np.zeros_like(grid)
+        grid_mask[start_x : end_x + 1, start_y : end_y + 1] = 1
+        white_mask = (grid == (255, 255, 255)).all(axis=2)[..., np.newaxis]
+        opponent_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+        opponent_mask = np.zeros_like(grid_mask)
+        for opponent_color in opponent_colors:
+            opponent_mask |= (grid == opponent_color).all(axis=2)[..., np.newaxis]
+            if (grid_mask & opponent_mask).any():
+                print("Selected area overlaps with opponent color.")
+                continue
+
+        grid_mask = np.zeros_like(grid)
+        grid_mask[start_x : end_x + 1, start_y : end_y + 1] = 1
+        white_mask = (grid == (255, 255, 255)).all(axis=2)[..., np.newaxis]
+        opponent_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+        opponent_mask = np.zeros_like(grid_mask)
+        for opponent_color in opponent_colors:
+            opponent_mask |= (grid == opponent_color).all(axis=2)[..., np.newaxis]
+            if (grid_mask & opponent_mask).any():
+                print("Selected area overlaps with opponent color.")
+                continue
+            elif (grid_mask & white_mask).sum() < selected_area:
+                print("Selected area overlaps with existing color.")
+                continue
+        reward = get_reward(grid, color, start_x, start_y, end_x, end_y)
+
+        if selected_area > color_total_area:
+            print("Selected area is greater than the color's total area.")
+            reward -= 0.5
+        elif (grid_mask & (grid != color).all(axis=2)[..., np.newaxis]).any():
+            print("Selected area contains opponent color.")
+            reward += 0.5
+            if selected_area == color_total_area:
+                reward += 1.0
+        elif (grid_mask & white_mask).sum() == selected_area:
+            print("Selected area contains only white spots.")
+            reward += 0.2
+        else:
+            print("Selected area is invalid.")
+            reward -= 0.2
+
+        probability = random.random()
+        if probability <= 0.999:
+            grid[start_x : end_x + 1, start_y : end_y + 1] = color
+            print("Area bought.")
+            return color_total_area, selected_area, reward
+        else:
+            grid_mask = np.zeros_like(grid)
             color_indices = np.where((grid == color).all(axis=2))
             color_indices = list(
-                zip(
-                    color_indices[0][:selected_area],
-                    color_indices[1][:selected_area],
-                )
+                zip(color_indices[0][:selected_area], color_indices[1][:selected_area])
             )
             for x, y in color_indices:
-                grid[x][y] = (255, 255, 255)
-    else:
-        print("Selected area cannot be greater than total color square.")
+                grid[x, y] = (255, 255, 255)
+            print("Area not bought.")
+            return color_total_area, 0, 0
 
+
+def select_action(model, state, actions):
+    state = np.array(state).reshape(1, -1)
+    try:
+        q_values = model.predict(state)
+        action = np.argmax(q_values[0])
+
+    except ValueError:
+        print("ValueError: Cannot select action.")
+    return actions[action]
+
+
+def get_reward(grid, color, start_x, start_y, end_x, end_y):
+    start_grid_x, start_grid_y = start_x // 10, start_y // 10
+    end_grid_x, end_grid_y = end_x // 10, end_y // 10
+    selected_area = (end_grid_x - start_grid_x + 1) * (end_grid_y - start_grid_y + 1)
+    total_color_square = np.sum((grid == color).all(axis=2))
+    white_mask = (grid == (255, 255, 255)).all(axis=2)
+    if selected_area > total_color_square:
+        reward = -2.0
+    elif (white_mask[start_x : end_x + 1, start_y : end_y + 1]).all():
+        reward = -1.0
+    else:
+        reward = 0
+        captured_opponent = False
+        for opponent_color in [(255, 0, 0), (0, 255, 0), (0, 0, 255)]:
+            opponent_mask = (grid == opponent_color).all(axis=2)
+            opponent_total_square = np.sum(opponent_mask)
+            if (
+                opponent_total_square > 0
+                and (opponent_mask[start_x : end_x + 1, start_y : end_y + 1]).any()
+            ):
+                captured_opponent = True
+                if selected_area == total_color_square:
+                    reward = 2.0
+                else:
+                    reward = 1.0
+                break
+        if not captured_opponent:
+            reward = -0.2
+    return reward
+
+
+def train_model(model, grid, color, actions, episodes):
+    color = np.array(color)
+    for episode in range(episodes):
+        state = get_state(grid, color)
+        action = select_action(model, state, actions)
+        start_x, start_y = action[0] * 10, action[1] * 10
+        end_x, end_y = action[2] * 10 + 9, action[3] * 10 + 9
+        ai_function(color, grid)
+        reward = get_reward(grid, color, start_x, start_y, end_x, end_y)
+        next_state = get_state(grid, color)
+        model.fit(
+            np.array(state).reshape(1, -1),
+            np.array(
+                [
+                    reward
+                    + 0.99 * np.max(model.predict(np.array(next_state).reshape(1, -1)))
+                ]
+            ),
+            epochs=1,
+            verbose=0,
+        )
+
+        if np.sum((grid == color).all(axis=2)) == 7000:
+            print(f"{color} has captured the whole map!")
+            break
+
+
+models = [Sequential() for _ in range(4)]
+for model in models:
+    model.add(Dense(24, input_dim=5, activation="relu"))  # Updated input_dim to 5
+    model.add(Dense(48, activation="relu"))
+    model.add(Dense(3, activation="linear"))
+    model.compile(loss="mse", optimizer=Adam(learning_rate=0.001))
+
+
+colors = [
+    np.array([255, 0, 0]),
+    np.array([0, 255, 0]),
+    np.array([0, 150, 255]),
+    np.array([255, 215, 0]),
+]
+for color, model in zip(colors, models):
+    train_model(model, grid, np.array(color), actions, episodes=100)
 
 while running:
     for event in pygame.event.get():
@@ -124,17 +317,18 @@ while running:
             return color
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+            for color, model in zip(colors, models):
+                state = get_state(grid, color)
+                action = select_action(model, state, actions)
+                start_x, start_y = action[0] * 10, action[1] * 10
+                end_x, end_y = action[2] * 10 + 9, action[3] * 10 + 9
+                ai_function(color, grid)
             start_color = set_color(grid, event.pos)
+
             start_pos = event.pos
             dragging = True
 
         if event.type == pygame.MOUSEBUTTONUP:
-            for i in [
-                top_right_color,
-                bottom_left_color,
-                bottom_right_color,
-            ]:
-                bot_function(i)
             end_pos = event.pos
             dragging = False
 
@@ -156,7 +350,7 @@ while running:
 
             if selected_area < total_color_square:
                 probability = random.random()
-                if probability <= 0.9:
+                if probability <= 0.99:
                     grid[
                         start_grid_x : end_grid_x + 1, start_grid_y : end_grid_y + 1
                     ] = start_color
@@ -173,8 +367,8 @@ while running:
             else:
                 print("Selected area cannot be greater than total color square.")
 
-        for x in range(50):
-            for y in range(50):
+        for x in range(100):
+            for y in range(100):
                 if event.type == pygame.MOUSEMOTION and dragging:
                     end_pos = event.pos
                     start_x, start_y = start_pos
